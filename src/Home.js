d42b3a7101cef45875d6589c029e79b7414f5092
junaidj4 <junaidjamshaid775@gmail.com>
2023-04-08 16:56:44 +0500

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+
 import Featured from "./components/Featured";
 import HeroSection from "./components/HeroSection";
 
--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,36 @@
 import React from "react";
 import Searchbar2 from "./components/Searchbar2";
-
+import ProductAd from "./components/ProductAd";
+import "./Search.css";
 const Search = () => {
   return (
     <>
       <div>
         <Searchbar2 />
+        <div className="ad-list">
+          <div className="left-side">
+            <p>Filter your search results</p>
+          </div>
+          <div className="right-side">
+            <div className="ad-section">
+              <p id="title">Sort By</p>
+              <p id="selected">Best-Sellers</p>
+              <p id="normal">Best-Match</p>
+              <p id="normal">Newest</p>
+              <p id="normal">Best rated</p>
+            </div>
+            <div className="ad-grid">
+              <ProductAd />
+              <ProductAd />
+              <ProductAd />
+              <ProductAd />
+              <ProductAd />
+              <ProductAd />
+              <ProductAd />
+              <ProductAd />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
