502686afe7fd8daed150c6bda9078e37c4e71fae
junaidj4 <junaidjamshaid775@gmail.com>
2023-04-17 20:20:15 +0500

    Filter Section

--- src/Search.js
+++ src/Search.js
@@ -9,7 +9,39 @@ const Search = () => {
         <Searchbar2 />
         <div className="ad-list">
           <div className="left-side">
-            <p>Filter your search results</p>
+            <h3 id="head">Filter your search results</h3>
+            <div className="filter">
+              <h3 id="top-heading">Refine</h3>
+              <hr
+                style={{
+                  background: "#01020B",
+                  color: "#01020B",
+                  borderColor: "#01020B",
+                  height: "1px",
+                  width: "100%",
+                }}
+              />
+              <h3>Topics</h3>
+              <p id="selected-filter">Admin Panel Templates</p>
+              <p>Blogs</p>
+              <p>Business</p>
+              <p>Construction</p>
+              <p>eCommerce</p>
+              <p>Education</p>
+              <p>Entertainment</p>
+              <p>Health</p>
+              <p>Mobile</p>
+              <hr
+                style={{
+                  background: "#01020B",
+                  color: "#01020B",
+                  borderColor: "#01020B",
+                  height: "1px",
+                  width: "100%",
+                }}
+              />
+              <h3>Price</h3>
+            </div>
           </div>
           <div className="right-side">
             <div className="ad-section">
